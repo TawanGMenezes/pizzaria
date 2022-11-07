b6ae553c0ab505002e0abaacd84cf0967a2d0ec3
TawanMenezes <tawanmenezes@gmail.com>
2022-11-07 15:44:56 -0300

    modal

--- script.js
+++ script.js
@@ -6,11 +6,33 @@ const cs = (el) => document.querySelectorAll(el);
 // 'pizza-item'
 pizzaJson.map((item, index) => {
   let pizzaItem = c('.pizza-item').cloneNode(true)
+  // definindo a constante de preço
+  const pizzaPrice = item.price.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
   //preencher as info das pizzas
   pizzaItem.querySelector('.pizza-item--img img').src = item.img
-  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `${pizzaPrice}`
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+  //inserido evento de click para aparecer o modal com efeito
+  //utilizando o timeout
+  pizzaItem.querySelector('a').addEventListener('click', (e) => {
+    e.preventDefault();
+    c('.pizzaWindowArea').style.opacity = 0;
+    c('.pizzaWindowArea').style.display = 'flex'
+    setTimeout(() => {
+      c('.pizzaWindowArea').style.opacity = 1;
+    }, 200);
+
+
+  })
+
+
+
+
+
   //inserindo o clone de acordo com o map na area das pizza
   c('.pizza-area').append(pizzaItem)
 })
