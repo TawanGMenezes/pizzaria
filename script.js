c4bde198c1ca9385338e3ab956e7f6700981d1a3
TawanMenezes <tawanmenezes@gmail.com>
2022-11-07 17:40:29 -0300

    implementando modal

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let modalQt = 1
 //Definindo as constantes para economizar codigo
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
@@ -6,20 +7,38 @@ const cs = (el) => document.querySelectorAll(el);
 // 'pizza-item'
 pizzaJson.map((item, index) => {
   let pizzaItem = c('.pizza-item').cloneNode(true)
-  // definindo a constante de preço
-  const pizzaPrice = item.price.toLocaleString("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  });
-  //preencher as info das pizzas
+  // setando o identificador nas pizzas
+  pizzaItem.setAttribute('data-key', index)
+  //preencheendo as info das pizzas
   pizzaItem.querySelector('.pizza-item--img img').src = item.img
-  pizzaItem.querySelector('.pizza-item--price').innerHTML = `${pizzaPrice}`
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
-  //inserido evento de click para aparecer o modal com efeito
-  //utilizando o timeout
+  //inserido evento de click para aparecer o modal
   pizzaItem.querySelector('a').addEventListener('click', (e) => {
     e.preventDefault();
+    //saindo do elemento 'a' e indo para o mais proximo chamado 'pizza-item' para
+    //acessar o atributo que foi setado onde identifica qual é a pizza
+    let key = e.target.closest('.pizza-item').getAttribute('data-key')
+    //definindo a quantidade que irá abrir no modal
+    modalQt = 1
+    //preenchendo as info do modal apos descobrir qual pizza foi clicada
+    c('.pizzaBig img').src = pizzaJson[key].img
+    c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+    c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+    c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`
+    //reseta a seleção de tamanho ao abrir o modal
+    c('.pizzaInfo--size.selected').classList.remove('selected')
+    //preenche o tamanho das pizzas no modal
+    cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+      //seta como selecionado toda pizza grande
+      if (sizeIndex == 2) {
+        size.classList.add('selected')
+      }
+      size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+    })
+    c('pizzaInfo--qt').innerHTML = modalQt
+    //modal com efeito utilizando o timeout
     c('.pizzaWindowArea').style.opacity = 0;
     c('.pizzaWindowArea').style.display = 'flex'
     setTimeout(() => {
